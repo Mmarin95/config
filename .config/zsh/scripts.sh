1d06a28fcc0c8e18249ef6da95ad423860d757b4
Mmarin95 <9479635+Mmarin95@users.noreply.github.com>
2024-02-08 19:58:14 +0100

    add lynx dunctions

--- .config/zsh/scripts.sh
+++ .config/zsh/scripts.sh
@@ -3,3 +3,11 @@
 compress() {
     tar cvzf $1.tar.gz $1
 }
+
+duckduckgo() {
+    lynx -vikeys -accept_all_cookies "https://lite.duckduckgo.com/lite/?q=$@"
+}
+
+wikipedia() {
+    lynx -vikeys -accept_all_cookies "https://en.wikipedia.org/wiki?search=$@"
+}
